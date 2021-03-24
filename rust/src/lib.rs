28871a3d4d94939522d49714bab589ead3d4342e
Jeff <JeffreyRiggle@gmail.com>
2021-03-23 21:31:12 -0400

    finish out rust stuff

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,5 +1,17 @@
-pub fn is_palindrome(_value: String) -> bool {
-    false
+fn clean_string(value: String) -> String {
+    value.to_lowercase()
+         .replace(" ", "")
+         .replace("'", "")
+         .replace(",", "")
+         .replace(".", "")
+         .replace("!", "")
+         .replace("?", "")
+}
+
+pub fn is_palindrome(value: String) -> bool {
+    let c_value: String = clean_string(value);
+    let r_value: String = c_value.chars().rev().collect::<String>();
+    r_value == c_value
 }
 
 #[cfg(test)]
--- rust/src/test.rs
+++ rust/src/test.rs
@@ -4,3 +4,43 @@ use super::*;
 fn test_not_palindrome() {
     assert_eq!(is_palindrome("test".to_string()), false);
 }
+
+#[test]
+fn test_simple_palindrome() {
+    assert_eq!(is_palindrome("level".to_string()), true);
+}
+
+#[test]
+fn test_palindrome_with_space() {
+    assert_eq!(is_palindrome("madam im adam".to_string()), true);
+}
+
+#[test]
+fn test_palindrome_with_case() {
+    assert_eq!(is_palindrome("Madam".to_string()), true);
+}
+
+#[test]
+fn test_palindrome_with_apostrope() {
+    assert_eq!(is_palindrome("Madam I'm Adam".to_string()), true);
+}
+
+#[test]
+fn test_palindrome_with_comma() {
+    assert_eq!(is_palindrome("Madam, I'm Adam".to_string()), true);
+}
+
+#[test]
+fn test_palindrome_with_period() {
+    assert_eq!(is_palindrome("Madam, I'm Adam.".to_string()), true);
+}
+
+#[test]
+fn test_palindrome_with_exclaimation() {
+    assert_eq!(is_palindrome("Madam, I'm Adam!".to_string()), true);
+}
+
+#[test]
+fn test_palindrome_with_question() {
+    assert_eq!(is_palindrome("Madam, I'm Adam?".to_string()), true);
+}
