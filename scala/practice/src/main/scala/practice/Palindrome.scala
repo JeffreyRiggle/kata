3eccd0911a61cb1ec647f2eef46e16495b6edc92
Jeff <JeffreyRiggle@gmail.com>
2021-04-09 22:53:41 -0400

    finish out scala test

--- scala/practice/src/main/scala/practice/Palindrome.scala
+++ scala/practice/src/main/scala/practice/Palindrome.scala
@@ -1,8 +1,11 @@
 package practice
 
 class Palindrome {
+    val cleanPattern = "[\\s',\\.!\\?]".r
+
     def isPalindrome(value: String) : Boolean = {
-        val revsersedValue = value.reverse
-        return value == revsersedValue
+        val cleanValue = cleanPattern.replaceAllIn(value.toLowerCase, "")
+        val revsersedValue = cleanValue.reverse
+        return cleanValue == revsersedValue
     }
 }
--- scala/practice/src/test/scala/practice/PalindromeSpec.scala
+++ scala/practice/src/test/scala/practice/PalindromeSpec.scala
@@ -14,4 +14,39 @@ class PalindromeSpec extends AnyFlatSpec {
         val result = palindrome.isPalindrome("level")
         assert(result)
     }
+
+    it should "handle palindromes with spaces" in {
+        val result = palindrome.isPalindrome("madam im adam")
+        assert(result)
+    }
+
+    it should "handle palindromes with casing" in {
+        val result = palindrome.isPalindrome("Madam")
+        assert(result)
+    }
+
+    it should "handle palindromes with apostrope" in {
+        val result = palindrome.isPalindrome("Madam I'm Adam")
+        assert(result)
+    }
+
+    it should "handle palindromes with comma" in {
+        val result = palindrome.isPalindrome("Madam, I'm Adam")
+        assert(result)
+    }
+
+    it should "handle palindromes with period" in {
+        val result = palindrome.isPalindrome("Madam, I'm Adam.")
+        assert(result)
+    }
+
+    it should "handle palindromes with exclamation" in {
+        val result = palindrome.isPalindrome("Madam, I'm Adam!")
+        assert(result)
+    }
+
+    it should "handle palindromes with question" in {
+        val result = palindrome.isPalindrome("Madam, I'm Adam?")
+        assert(result)
+    }
 }
